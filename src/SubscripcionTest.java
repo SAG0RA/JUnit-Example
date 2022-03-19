991fa0984e611323327dfb4f65eb3f8817393722
Saul Gomez Ramirez <82982124+SAG0RA@users.noreply.github.com>
2022-03-18 18:27:30 -0600

    Actualizacion

--- src/SubscripcionTest.java
+++ src/SubscripcionTest.java
@@ -5,8 +5,18 @@ import org.junit.jupiter.api.Test;
 class SubscripcionTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testprecioPorMes() {
+		double esperado = 1;
+		Subscripcion s = new Subscripcion(200, 2);
+		double resultado = s.precioPorMes();
+		assertEquals(esperado, resultado, 0);
 	}
 
+	@Test
+	public void testprecioPorMes2() {
+		double esperado = 67;
+		Subscripcion s = new Subscripcion(200, 3);
+		double resultado = s.precioPorMes();
+		assertEquals(esperado, resultado, 0);
+	}
 }
